d1d8dc94896eef5e9054364449437132af0c3b1e
Koa <13078659+koaie@users.noreply.github.com>
2021-09-01 06:22:11 +0100

    remove hmr

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import HMR from "@roxi/routify/hmr";
 import App from "./App.svelte";
 import "./main.css";
 
-const app = HMR(App, { target: document.body }, "anisoc");
+const app = new App({
+  target: document.body,
+});
 
 export default app;
