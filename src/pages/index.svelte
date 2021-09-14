6866016138badd03317ad275ccdae834e8ffefdf
Koa <13078659+koaie@users.noreply.github.com>
2021-09-14 12:19:52 +0100

    index main text box change text

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -17,9 +17,30 @@
     <Headline>Anime & Animation Society</Headline>
     <Subhead>The University of Edinburgh</Subhead>
     <p>
-      Dedicated to sharing Japanese animation – anime – with students of the
-      university, we provide weekly showings suitable for both complete
-      newcomers as well as veterans of the hobby.
+      The Anime and Animation society is an appreciation society for all
+      Japanese (and inspired) media, primarily anime. We want to provide a warm
+      community and a chill space with both relaxing and out-there anime. With
+      weekly online screenings (and in-person casual meet-ups), we hope to give
+      you a good variety of shows to explore and an opportunity to make new
+      friends and discover all things anime and manga! We have many upcoming
+      events throughout the year, primarily at Halloween and Christmas, and
+      smaller events will be scattered in both semesters, covering a range of
+      activities. In the past, we've done everything from bowling to ice-skating
+      to karaoke to bubble tea mixing!
+    </p>
+    <p>
+      We know everyone has been through a rough time with COVID, and we as a
+      society want to continue to be a safe place where everyone comes to relax.
+      As such, we’re hosting screenings online for semester 1, and doing a mix
+      of virtual and IRL events over this coming year. All our in-person events
+      will follow COVID safety guidelines - sanitiser, facemasks, and social
+      distancing when indoors.
+    </p>
+    <p>
+      We screen a variety of genres throughout the year, so that we can make
+      sure there’s something for everyone; from action shounen shows, to
+      rom-coms, to out-there sci-fi, we watch it all, so come on down to our
+      discord group!
     </p>
   </Card>
 </div>
